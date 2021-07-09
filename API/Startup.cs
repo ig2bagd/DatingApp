7e08dc60a1e416b22092b88de751c9039f9d2dcf
steviep <ig2bagd@yahoo.com>
2021-07-08 22:31:11 -0700

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -18,7 +18,6 @@ namespace API
 {
     public class Startup
     {
-
         private readonly IConfiguration _config;
 
         public Startup(IConfiguration config)
@@ -35,6 +34,7 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -54,6 +54,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
